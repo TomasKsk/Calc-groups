bb5e0ac6832c4b59e756e98a0310c6db3c21e933
Tomáš Krže <106106200+TomasKsk@users.noreply.github.com>
2024-04-27 11:59:02 +0200

    window Width, media queries, bug fixing
    The tailwind media queries didn't work properly so I added functions to get the page width on each render so if the user is playing with resizing the window the right icons appear and css is applied.
    
    New and better conditions for the save and menu icons in the display component.
    
    Bug fixing - the new conditions prevent the user to be able to save the calculations when last item in the display.mem array is not a number or the array is empty

--- src/Components/Display.jsx
+++ src/Components/Display.jsx
@@ -1,5 +1,21 @@
+import { useState, useEffect } from 'react';
+
 const Display = ({ display, setDisplay, storage, setStorage, storeIcon, setStoreIcon }) => {
-    let pageWidth = document.getElementById('root').offsetWidth;
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+  
+    useEffect(() => {
+        console.log(window.innerWidth)
+        // Add event listener to update window width when the window is resized
+        window.addEventListener('resize', handleResize);
+    
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }); // run on each render
 
     // saving to calcStorage
     const handleSave = () => {
@@ -25,7 +41,13 @@ const Display = ({ display, setDisplay, storage, setStorage, storeIcon, setStore
         setStoreIcon(prev => !prev)
     }
 
-    return(
+    // Condition for rendering the save icon SVG
+    const renderSaveIcon = display.mem.length > 0 && !isNaN(display.mem[display.mem.length - 1]);
+
+    // Condition for rendering the storage icon button
+    const renderStorageIcon = window.innerWidth <= 750;
+
+    return (
         <div className='overflow-hidden flex flex-col background w-full min-w-350 min-h-32 min-[750px]:h-60 justify-center items-center bg-gradient-to-r border-double border-8 border-gray-800 rounded-t-xl relative'>
             <h1 className='font-bold text-lg min-[750px]:text-4xl text-right pr-2 absolute top-2 right-2'>
                 {/** display memmory */}
@@ -36,26 +58,29 @@ const Display = ({ display, setDisplay, storage, setStorage, storeIcon, setStore
                 {display.sum}
             </h2>
             {/* save icon svg */}
-
-            <button onClick={() => handleSave()} className='font-bold absolute bottom-2 right-2 hover:translate-y-1'>
-                {display.mem.length > 0 && <svg className="h-8 w-8 text-slate-500 hover:stroke-black" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z" />  <polyline points="17 21 17 13 7 13 7 21" />  <polyline points="7 3 7 8 15 8" /></svg>}
-            </button>
-            {/* storage icon */}
-            {/** render upon width of parent */}
-            {
-                // (pageWidth < 750) &&
-                <button onClick={() => handleMenu()} className='absolute top-2 left-2 flex h-6 w-6 font-bold text-gray-500 justify-center'>
-                    {(!storeIcon) ? 
-                        <svg className="h-6 w-6 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"/>
-                        </svg>
-                        : 'X'
-                    }
+            {renderSaveIcon && (
+                <button onClick={handleSave} className='font-bold absolute bottom-2 right-2 hover:translate-y-1'>
+                    <svg className="h-8 w-8 text-slate-500 hover:stroke-black" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                        <path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z" />
+                        <polyline points="17 21 17 13 7 13 7 21" />
+                        <polyline points="7 3 7 8 15 8" />
+                    </svg>
                 </button>
-            }
-
+            )}
+            {/* storage icon */}
+            {renderStorageIcon && (
+                <button onClick={() => handleMenu()} className='absolute top-2 left-2 flex h-6 w-6 font-bold text-gray-500 justify-center'>
+                {!storeIcon ? (
+                    <svg className="h-6 w-6 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"/>
+                    </svg>
+                ) : (
+                    'X'
+                )}
+                </button>
+            )}
         </div>
-    )
+    );
 };
 
 
--- src/Components/Storage.jsx
+++ src/Components/Storage.jsx
@@ -1,11 +1,41 @@
+import { useState, useEffect } from 'react';
 import Item from "./Storage/Item";
 
 const Storage = ({ setDisplay, storage, setStorage, storeIcon }) => {
-    const storegePos = !storeIcon ? 'full' : '0';
+
+    const [mediaStyle, setMediaStyle] = useState({});
+
+    // instead of direct media queries because they werent stable
+    useEffect(() => {
+        console.log(window.innerWidth)
+        const handleResize = () => {
+            if (window.innerWidth <= 750) {
+                setMediaStyle({
+                    transition: 'right 0.7s',
+                    height: '80%',
+                    bottom: '0%',
+                    position: 'absolute',
+                    right: !storeIcon ? '100%' : '0%'
+                });
+            } else {
+                setMediaStyle({});
+            }
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        // Initial call to set media style based on window width
+        handleResize();
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, [storeIcon]);
 
     let [first, second, third] = [{}, {}, {}];
     let arr = Object.keys(storage);
 
+    // sort incoming storage items to collumns
     arr.forEach((key, index) => {
         if (index % 3 === 0) {
             first[key] = storage[key];
@@ -18,12 +48,14 @@ const Storage = ({ setDisplay, storage, setStorage, storeIcon }) => {
 
     return(
         <div 
-            className={`flex flex-col items-center relative min-w-300 size-full p-2 right-0 max-[750px]:right-${storegePos} max-[750px]:bottom-0 max-[750px]:h-4/5 max-[750px]:absolute bg-gray-200/90 bg-gradient-to-b from-slate-300/50 border-gray-600 border-solid border-8 rounded-t-xl`}
-            style={{transition: 'right 0.7s'}}
+        // {`flex flex-col items-center relative min-w-300 size-full p-2 max-[750px]:right-${storegePos} max-[750px]:bottom-0 max-[750px]:h-4/5 max-[750px]:absolute bg-gray-200/90 bg-gradient-to-b from-slate-300/50 border-gray-600 border-solid border-8 rounded-t-xl`}
+            className={`flex flex-col items-center relative min-w-300 size-full p-2 bg-gray-200/90 bg-gradient-to-b from-slate-300/50 border-gray-600 border-solid border-8 rounded-t-xl`}
+            style={mediaStyle}
         >
                 <h1 className='font-black text-xl'>Calculator Storage</h1>
 
                 <div style={{scrollbarWidth: 'thin'}} className='flex flex-row max-[350px]:flex-col justify-center gap-2 p-2 overflow-y-auto'>
+
                     {/* Column 1 */}
                     <div className='flex flex-col gap-2 w-auto'>
                         {
